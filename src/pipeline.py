d9d61cc094939de1579df088b3de86bfe03d2f74
Tim Kreutz <tjkreutz@gmail.com>
2017-06-25 12:54:10 +0200

    Output cv accuracy for binary classifiers with a basic pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,32 +3,43 @@ import os
 from collections import defaultdict
 from helpers.dataselection import *
 from sklearn.svm import LinearSVC
+from sklearn.cross_validation import cross_val_score
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def train_binary_cls(file_list, labels):
+def get_pipeline_configuration():
 
-    training_files = [open(file, 'r').read() for file in file_list]
-
+    clf = LinearSVC()
     pipeline = Pipeline([
-            ('features', FeatureUnion([
-                ('tfidf', TfidfVectorizer(ngram_range=(1, 1))),
-            ])),
-            ('classifier', LinearSVC())
+        ('features', FeatureUnion([
+            ('tfidf', TfidfVectorizer(ngram_range=(1, 1))),
+        ])),
+        ('classifier', clf)
     ])
 
-    pipeline.fit_transform(training_files, labels)
     return pipeline
 
 
-def generate_binary_labels(labelfile):
+def get_baseline(labels):
+    return max(labels.count(0), labels.count(1)) / len(labels)
+
+
+def train_test_binary_cls(pipeline, file_list, labels):
+
+    training_files = [open(file, 'r').read() for file in file_list]
+    scores = cross_val_score(pipeline, training_files, labels)
+
+    return scores
+
+
+def generate_binary_labels(datadir, labelfile):
     file_list = []
     binary_labels = defaultdict(list)
     line_index = 0
     for line in labelfile.readlines():
         file, labels = line.split('\t')
-        file_list.append(file)
+        file_list.append(os.path.join(datadir, file))
         labels = [label.strip() for label in labels.split(',')]
         for label in labels:
             index_diff = line_index - len(binary_labels[label])
@@ -52,10 +63,14 @@ def main(datadir):
 
     labelfile = open(os.path.join(datadir, 'labelfile.csv'), 'r')
 
-    file_list, binary_labels = generate_binary_labels(labelfile)
+    file_list, binary_labels = generate_binary_labels(datadir, labelfile)
+    pipeline = get_pipeline_configuration()
 
-    for labels in binary_labels.items():
-        train_binary_cls(file_list, labels)
+    for label, values in binary_labels.items():
+        baseline = get_baseline(values)
+        print('Baseline accuracy for label {0}: {1}'.format(label, baseline))
+        scores = train_test_binary_cls(pipeline, file_list, values)
+        print('Crossvalidation accuracy for label {0}: {1}'.format(label, scores.mean()))
 
 
 if __name__ == '__main__':
