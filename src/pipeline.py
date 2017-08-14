ead3ac84367fbfeda2b8c090e4d9498b054963b2
Tim Kreutz <tjkreutz@gmail.com>
2017-08-14 17:20:30 +0200

    Streamline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,8 @@
 import sys
 import os
-import numpy as np
 from features import *
 from util import read_labelfile
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score
@@ -31,40 +28,6 @@ def get_pipeline_configuration():
     ])
 
     return pipeline
-
-
-def show_plot(clf, vec, training_features, training_labels, testing_features, testing_labels):
-    # Plotting decision regions
-
-    importances = clf.feature_importances_
-
-    indices = np.argsort(importances)[::-1]
-    used_features = indices[:2]
-    feature_names = vec.get_feature_names()
-    two_feature_names = [feature_names[i] for i in used_features]
-
-    two_training_features = training_features[:, used_features].todense()
-    two_testing_features = testing_features[:, used_features].todense()
-
-    clf = DecisionTreeClassifier()
-    clf.fit(two_training_features, training_labels)
-
-    x_min, x_max = two_training_features[:, 0].min() - 1, two_training_features[:, 0].max() + 1
-    y_min, y_max = two_training_features[:, 1].min() - 1, two_training_features[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
-                         np.arange(y_min, y_max, 0.1))
-
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-
-    ar = plt.subplot()
-
-    ar.scatter(two_testing_features[:, 0], two_testing_features[:, 1], c=testing_labels, s=20, edgecolor='k')
-    ar.contourf(xx, yy, Z, alpha=0.4)
-    ar.set_xlabel(two_feature_names[0])
-    ar.set_ylabel(two_feature_names[1])
-
-    plt.show()
 
 
 def main(datadir):
@@ -96,8 +59,6 @@ def main(datadir):
         precision_score(testing_labels, pred, average='macro'),
         recall_score(testing_labels, pred, average='macro'),
         f1_score(testing_labels, pred, average='macro')))
-
-#    show_plot(clf, vec, training_features, training_labels, testing_features, testing_labels)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
