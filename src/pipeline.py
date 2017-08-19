37ef426a46217a8417b5660252d1a4a267664bd4
Tim Kreutz <tjkreutz@gmail.com>
2017-08-19 17:19:22 +0200

    Set up evaluation in a better way

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,11 @@
 import sys
 import os
+from eval import *
 from features import *
 from util import read_labelfile
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -15,14 +15,14 @@ def get_pipeline_configuration():
     clf = OneVsRestClassifier(DecisionTreeClassifier())
     pipeline = Pipeline([
         ('features', FeatureUnion([
-#            ('countadjectives', CountAdjectives()),
-#            ('taggedwords', TaggedWords()),
-#            ('skipgrams', SkipgramVectorizer(n=3, k=2, max_df=0.9)),
+            ('countadjectives', CountAdjectives()),
+            ('taggedwords', TaggedWords()),
+            ('skipgrams', SkipgramVectorizer(n=3, k=2, max_df=0.9)),
             ('countvectorizer', CountVectorizer(ngram_range=(1, 3), max_df=0.4, input='filename')),
-#            ('documentlength', DocumentLength()),
-#            ('typetokenratio', TypeTokenRatio()),
-#            ('numberofparagraphs', NumberOfParagraphs()),
-#            ('rechtbank', Rechtbank()),
+            ('documentlength', DocumentLength()),
+            ('typetokenratio', TypeTokenRatio()),
+            ('numberofparagraphs', NumberOfParagraphs()),
+            ('rechtbank', Rechtbank()),
         ])),
         ('classifier', clf)
     ])
@@ -42,23 +42,14 @@ def main(datadir):
     labels = []
 
     for filename, labelset in label_dict.items():
-        files.append(os.path.join(datadir, filename))
+        files.append(open(os.path.join(datadir, filename), 'r', encoding='utf-8').read())
         labels.append(labelset)
 
     labels = MultiLabelBinarizer().fit_transform(labels)
 
     pipeline = get_pipeline_configuration()
-    split = round(0.75*len(files))
-    training_files, testing_files = files[:split], files[split:]
-    training_labels, testing_labels = labels[:split], labels[split:]
-
-    pipeline.fit(training_files, training_labels)
-    pred = pipeline.predict(testing_files)
-
-    print('Precision:\t{0}\nRecall:\t{1}\nF1:\t{2}'.format(
-        precision_score(testing_labels, pred, average='macro'),
-        recall_score(testing_labels, pred, average='macro'),
-        f1_score(testing_labels, pred, average='macro')))
+    evaluator = NFoldEvaluator(pipeline, files, labels, 2)
+    evaluator.evaluate()
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
