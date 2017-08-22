3479ba1510aa114f09f60ba461eff85bd6095a05
Tim Kreutz <tjkreutz@gmail.com>
2017-08-22 12:56:59 +0200

    Update download script with features

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,6 @@
 wget "http://www.timkreutz.nl/files/small.tar.gz"
 wget "http://www.timkreutz.nl/files/large.tar.gz"
+wget "http://www.timkreutz.nl/files/features.tar.gz"
 tar -zxvf small.tar.gz
 tar -zxvf large.tar.gz
+tar -zxvf features.tar.gz
